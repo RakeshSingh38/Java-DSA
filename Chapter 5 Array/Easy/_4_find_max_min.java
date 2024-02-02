448873a07090fc433270f9652c661243dd3431c3
Rakesh Singh <rakeshsingh432165@gmail.com>
2024-02-02 13:11:07 +0530

    go

--- Chapter 5 Array/Easy/_4_find_max_min.java
+++ Chapter 5 Array/Easy/_4_find_max_min.java
@@ -13,7 +13,7 @@ public class _4_find_max_min {
         int min = arr[0];
 
         // for maximum
-        int max = arr[0];
+        int max = arr[0];        
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < min) {
@@ -53,6 +53,6 @@ public class _4_find_max_min {
             }
         }
         return min + max;
-        
+
     }
 }
